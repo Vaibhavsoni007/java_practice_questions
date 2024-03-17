e804d86211549db4a6133c7b9713a027c5c5bb7a
vaibhav soni <136541227+Vaibhavsoni007@users.noreply.github.com>
2024-03-17 16:36:24 +0530

    Add files via upload

---
+++ Exercise01.java
@@ -0,0 +1,11 @@
+package w3resource_java_problems.java_exercises_basic_part01;
+import java.util.*;
+public class Exercise01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your name");
+        String name = sc.nextLine();
+        System.out.println("hello");
+        System.out.println(name);
+    }
+}
