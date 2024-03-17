2cb1ebc652d5fe6ce99f347757b77769d10250da
vaibhav soni <136541227+Vaibhavsoni007@users.noreply.github.com>
2024-03-17 16:44:04 +0530

    Update Exercise03.java

--- Exercise03.java
+++ Exercise03.java
@@ -1,7 +1,6 @@
+//Write a Java program to divide two numbers and print them on the screen
 package w3resource_java_problems.java_exercises_basic_part01;
-
 import java.util.Scanner;
-
 public class Exercise03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
