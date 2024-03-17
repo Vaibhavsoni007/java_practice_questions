0cff1d69aaa02be8db427175f8762fce0e611e36
vaibhav soni <136541227+Vaibhavsoni007@users.noreply.github.com>
2024-03-17 17:23:56 +0530

    Add files via upload

---
+++ Exercise09.java
@@ -0,0 +1,12 @@
+//Write a Java program to compute the specified expressions and print the output.
+//((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+package w3resource_java_problems.java_exercises_basic_programs;
+
+import java.util.Scanner;
+
+public class Exercise09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
+    }
+}
