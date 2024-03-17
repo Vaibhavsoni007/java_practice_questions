c9ad4d75c0ae5fbc2aca672e7b091c45342e596a
vaibhav soni <136541227+Vaibhavsoni007@users.noreply.github.com>
2024-03-17 16:40:03 +0530

    Update Exercise01.java

--- Exercise01.java
+++ Exercise01.java
@@ -1,3 +1,4 @@
+//Write a Java program to print 'Hello' on screen and your name on a separate line
 package w3resource_java_problems.java_exercises_basic_part01;
 import java.util.*;
 public class Exercise01 {
