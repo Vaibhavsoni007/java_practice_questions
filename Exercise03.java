ffc3d72b769e66c2c1fd7912388ce1ce27ea97aa
vaibhav soni <136541227+Vaibhavsoni007@users.noreply.github.com>
2024-03-17 16:42:22 +0530

    Add files via upload

---
+++ Exercise03.java
@@ -0,0 +1,9 @@
+package w3resource_java_problems.java_exercises_basic_part01;
+
+import java.util.Scanner;
+
+public class Exercise03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
