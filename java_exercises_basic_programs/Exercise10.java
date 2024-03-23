64b457e0921e48866bd502db323be7abe11d2292
vaibhav soni <136541227+Vaibhavsoni007@users.noreply.github.com>
2024-03-23 14:46:18 +0530

    Delete java_exercises_basic_programs directory

--- java_exercises_basic_programs/Exercise10.java
+++
@@ -1,9 +0,0 @@
-//Write a Java program to compute a specified formula.
-//Specified Formula :
-//4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))
-package w3resource_java_problems.java_exercises_basic_programs;
-public class Exercise10 {
-    public static void main(String[] args) {
-        System.out.println(4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)));
-    }
-}
